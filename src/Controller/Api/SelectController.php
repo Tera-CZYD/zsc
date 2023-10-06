d1e4dbb2eac4cd61c12f96cac33286e29396a98e
Raymond Llobrera <monllobrera2514@gmail.com>
2023-10-06 08:39:56 +0800

    raymond

--- src/Controller/Api/SelectController.php
+++ src/Controller/Api/SelectController.php
@@ -206,13 +206,6 @@ class SelectController extends AppController {
     $this->loadModel('CollegeProgramSubs');
 
     $this->loadModel('YearLevelTerms');
-<<<<<<< HEAD
-=======
-
-    
-
-    //sir raf
->>>>>>> bee6c6cadf87755a3238d9e961d568eabaf6562b
 
     $this->loadModel("Apartelles");
 
@@ -2102,14 +2095,8 @@ class SelectController extends AppController {
         $year_term = "AND YearLevelTerm.chronological_order < $order";
 
       }
-<<<<<<< HEAD
 
       $sql = "
-=======
-      
-      $sql = "
-
->>>>>>> bee6c6cadf87755a3238d9e961d568eabaf6562b
 
         SELECT 
 
